15d7801b8fcd4d16c0017ecc11dfb17074d5aa73
Rabliel <58891547+Rabliel@users.noreply.github.com>
2021-05-08 21:42:28 +0200

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,7 @@
+const hamburger = document.getElementById('hamburger');
+const menu = document.getElementById('menu-list');
+
+
+hamburger.addEventListener('click',()=>{
+    menu.classList.toggle('header__item--show');
+});
