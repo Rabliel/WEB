24c664b4469df7075e8e1322caa876d5bfd1e1c8
Rabliel <58891547+Rabliel@users.noreply.github.com>
2021-05-10 20:07:27 +0200

    Add files via upload

--- js/lightbox.js
+++ js/lightbox.js
@@ -5,7 +5,6 @@ document.body.appendChild(lightbox);
 const galleryOverlays=document.getElementsByClassName("gallery-item__overlay");
 const galleryImgs=document.getElementsByClassName("gallery-item__img");
 
-
 for( let i=0;i<galleryOverlays.length;i++){
     galleryOverlays[i].addEventListener('click',e=>{
             lightbox.classList.add("active");
@@ -16,6 +15,24 @@ for( let i=0;i<galleryOverlays.length;i++){
 
             const img = document.createElement("img");
             img.src=galleryImgs[i].src;
+            lightbox.appendChild(img);
+    });
+}
+
+
+const galleryInfos=document.getElementsByClassName("gallery-info");
+const galleryHomeImgs=document.getElementsByClassName("gallery-img");
+
+for( let i=0;i<galleryInfos.length;i++){
+    galleryInfos[i].addEventListener('click',e=>{
+            lightbox.classList.add("active");
+
+            while(lightbox.firstChild){
+                lightbox.removeChild(lightbox.firstChild);
+            }
+
+            const img = document.createElement("img");
+            img.src=galleryHomeImgs[i].src;
             lightbox.appendChild(img);
     });
 }
